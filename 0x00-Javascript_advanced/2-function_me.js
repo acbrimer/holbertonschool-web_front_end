c5948b842336da7c6aede118a1c959e5cd3d9989
Adam <arbimer@mac.com>
2021-09-30 11:15:54 -0500

    Added task 2

---
+++ 0x00-Javascript_advanced/2-function_me.js
@@ -0,0 +1,9 @@
+// 2-function_me
+
+function welcomeMessage(fullName) {
+  window.alert(`Welcome ${fullName}`);
+}
+
+const guillaume = () => welcomeMessage('Guillaume');
+const alex = () => welcomeMessage('Alex');
+const fred = () => welcomeMessage('Fred');
